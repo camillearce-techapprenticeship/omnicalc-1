184e728bdc647fd4fa49494fed29624ef591957f
Camille Arce <thecamillearce@gmail.com>
2023-03-30 20:26:04 +0000

    initial commit added square form and routed needed URLs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,33 @@
 class ApplicationController < ActionController::Base
+  def square_form
+    render({ :template => "calculation_templates/square_form.html.erb" })
+  end
 
+  def square_results
+    render({ :template => "calculation_templates/square_results.html.erb" })
+  end
+
+  def squareroot_form
+    render({ :template => "calculation_templates/squareroot_form.html.erb" })
+  end
+
+  def squareroot_results
+    render({ :template => "calculation_templates/squareroot_results.html.erb" })
+  end
+
+  def payment_form
+    render({ :template => "calculation_templates/payment_form.html.erb" })
+  end
+
+  def payment_results
+    render({ :template => "calculation_templates/payment_results.html.erb" })
+  end
+
+  def random_form
+    render({ :template => "calculation_templates/random_form.html.erb" })
+  end
+
+  def random_results
+    render({ :template => "calculation_templates/random_results.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-
+  get("/square/new", { :controller => "application", :action => "square_form" })
+  get("/square/results", { :controller => "application", :action => "square_results" })
+  get("square_root/new", { :controller => "application", :action => "squareroot_form" })
+  get("square_root/results", { :controller => "application", :action => "squareroot_results" })
+  get("payment/new", { :controller => "application", :action => "payment_form" })
+  get("payment/results", { :controller => "application", :action => "payment_results" })
+  get("random/new", { :controller => "application", :action => "random_form" })
+  get("/random/results", { :controller => "application", :action => "random_results" })
+   
 end
